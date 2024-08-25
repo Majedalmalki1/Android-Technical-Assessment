1ac715f49d746e218725f6ff9ceb67f83264a491
10M <145940805+Majedalmalki1@users.noreply.github.com>
2024-08-25 12:02:35 +0300

    Update myRootBypass.js

--- Scripts/myRootBypass.js
+++ Scripts/myRootBypass.js
@@ -1,9 +1,11 @@
 Java.perform(function() {
     var RootChecker = Java.use("com.tiagorlampert.rootcheck.util.RootChecker");
+	 // Hooking checkRootMethodOne
     RootChecker.checkRootMethodOne.implementation = function() {
 	console.log("checkRootMethodOne Bypassed")
         return false;
     };
+	 // Hooking checkRootMethodTwo
     RootChecker.checkRootMethodTwo.implementation = function() {
 	console.log("checkRootMethodTwo Bypassed")
         return false;
